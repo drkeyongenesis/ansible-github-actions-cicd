36c04b29571374e3cf94b1ae00cea2b6713598af
Genesis <keyongenesis@gmail.com>
2025-02-04 16:12:42 +0800

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "=================================================="
+echo "🎉 CI/CD Github Actions Ansible Image"
+echo ""
+echo "Welcome"
+echo ""
+echo "📚 For more information:"
+echo "  - This project: https://github.com/drkeyongenesis/ansible-github-actions-cicd"
+echo "  - Official docs: https://ansible.readthedocs.io/projects/ansible-build-data"
+echo "=================================================="
+echo ""
+
+echo "📦 Essential packages installed by install-essential.sh:"
+echo "=================================================="
+cat /app.sh
+echo "=================================================="
+echo ""
+
+echo "🐍 Python packages installed from requirements.txt:"
+echo "=================================================="
+cat /requirements.txt
+echo "=================================================="
+echo ""
+
+echo "ℹ️  Installed Ansible version:"
+echo "=================================================="
+ansible --version
+echo "=================================================="
+echo ""
+
+echo "💡 Suggested commands:"
+echo "=================================================="
+echo "- ansible          : Run ansible commands"
+echo "- ansible-playbook : Execute ansible playbooks"
+echo "- ansible-galaxy   : Manage Ansible roles and collections"
+echo "=================================================="
+
+# Keep container running
+exec "$@" 
